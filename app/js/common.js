fa2c0f1d40bdaae1afa5649fcb6ff3384b34b4fe
Posokhov Stas <stsposoh@gmail.com>
2016-08-20 20:29:20 +0300

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -10,23 +10,6 @@
     });
   };
 
-  //E-mail Ajax Send
-  $("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function() {
-      alert("Thank you!");
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-
 
   //Запрет перетаскивать картинки
   $("img, a").on("dragstart", function(event) {
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
 'use strict';
 
-const gulp         = require('gulp');
-const watch        = require('gulp-watch');
-const stylus       = require('gulp-stylus');
-const nib          = require('nib');  //библиотека миксинов для stylus
-const debug        = require('gulp-debug');  //для отлова ошибок
-const plumber      = require('gulp-plumber');
-const rename       = require('gulp-rename');
-const browserSync  = require('browser-sync').create();
-const sourcemaps   = require('gulp-sourcemaps');
-const newer        = require('gulp-newer');
-const notify       = require('gulp-notify');
-const cssnano      = require('gulp-cssnano');
-const concat       = require('gulp-concat');
-const uglify       = require('gulp-uglify');
-const cache        = require('gulp-cache'); // Подключаем библиотеку кеширования
-const babel        = require('gulp-babel');
-
+const gulp           = require('gulp');
+const watch          = require('gulp-watch');
+const stylus         = require('gulp-stylus');
+const nib            = require('nib');  //библиотека миксинов для stylus
+const debug          = require('gulp-debug');  //для отлова ошибок
+const plumber        = require('gulp-plumber');
+const rename         = require('gulp-rename');
+const browserSync    = require('browser-sync').create();
+const sourcemaps     = require('gulp-sourcemaps');
+const newer          = require('gulp-newer');
+const notify         = require('gulp-notify');
+const cssnano        = require('gulp-cssnano');
+const concat         = require('gulp-concat');
+const uglify         = require('gulp-uglify');
+const cache          = require('gulp-cache'); // Подключаем библиотеку кеширования
+const babel          = require('gulp-babel');
+const gutil          = require('gulp-util' );
+const del            = require('del');
+const imagemin       = require('gulp-imagemin');
+const pngquant       = require('imagemin-pngquant');
+const fileinclude    = require('gulp-file-include');  //вставляет стили из header.min.css в index.html
+const gulpRemoveHtml = require('gulp-remove-html');
+const ftp            = require('vinyl-ftp');
 
 
 gulp.task('styles', function () {
@@ -28,13 +34,13 @@ gulp.task('styles', function () {
       }))
     }))
     .pipe(sourcemaps.init())
-    //.pipe(debug({title: 'src'}))
+    .pipe(debug({title: 'src'}))
     .pipe(stylus({
       use:[nib()],
       'include css': true
     }))
-    //.pipe(debug({title: 'stylus'}))
-    //.pipe(cssnano())  //раскоментируйте если нужно сжать css
+    .pipe(debug({title: 'stylus'}))
+    .pipe(cssnano())  //сжать css
     .pipe(rename({suffix: '.min', prefix : ''}))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/css'));
@@ -59,7 +65,7 @@ gulp.task('js', function () {
     .pipe(babel({
       presets: ['es2015']
     }))
-    //.pipe(uglify())   //расскоментируйте для сжатия common.js
+    .pipe(uglify())   //сжать common.js
     .pipe(gulp.dest('dist/js'));
 });
 
@@ -67,17 +73,16 @@ gulp.task('js', function () {
 gulp.task('libs', function () {
   return gulp.src([
     //все js библиотеки подключать сюда
+	'app/assets/libs/jquery/dist/jquery.min.js',
     'app/assets/libs/es5-shim/es5-shim.min.js',
     'app/assets/libs/es5-shim/es5-sham.min.js',
-    'app/assets/libs/jquery/dist/jquery.min.js',
     'app/assets/libs/modernizr/modernizr.min.js'
-    //'app/assets/libs/owl.carousel/owl.carousel.min.js'
+    //'app/assets/libs/owl.carousel/owl.carousel.min.js',
     //'app/assets/libs/jQuery.equalHeights/jquery.equalheights.min.js',
     //'app/assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
     //'app/assets/libs/animateNumber/jquery.animateNumber.min.js',
     //'app/assets/libs/waypoints/lib/jquery.waypoints.min.js',
-    //'app/assets/libs/masonry/dist/masonry.pkgd.min.js',
-    //'app/assets/libs/flipclock/compiled/flipclock.min.js'
+    //'app/assets/libs/masonry/dist/masonry.pkgd.min.js'
   ])
   .pipe(plumber({
     errorHandler: notify.onError(err => ({
@@ -109,6 +114,42 @@ gulp.task('browser-sync', function() {
   });
   browserSync.watch('dist/**/*.*').on('change', browserSync.reload);
 });
+
+
+gulp.task('removedist', function() { 
+  return del('dist'); 
+});
+
+
+gulp.task('removeHeaderCSS', function() { 
+  return del(['dist/css/header.min.css']); 
+});
+
+
+gulp.task('buildhtml', function() {
+  return gulp.src(['dist/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@'
+    }))
+    .pipe(gulpRemoveHtml())
+    .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('imagemin', function() {
+	return gulp.src('dist/img/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/img')); 
+});
+
+
+//production
+gulp.task('production', gulp.series('removedist', 'assets', 'libs', 'styles', 'js', 'buildhtml', 'imagemin', 'removeHeaderCSS'));
 
 
 gulp.task('default', 
